9e7100286681484d34975d200bd53230bedd14d4
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-09-02 23:42:41 +0000

    added sass compiler

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,27 @@
-grunt.initConfig({
-  sass: {
-    dist: {
-      files: {
-        'style.css': 'style.scss'
+module.exports = function(grunt){
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.scss'
+        }
       }
-    }
-  },
+    },
+    // Riss working up untill here
+
+
+
+    // Sophie working up untill here
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('checkSASS', ['sass']);
+
   // Riss working up untill here
 
 
 
   // Sophie working up untill here
-});
-
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.registerTask('default', ['sass']);
-
-// Riss working up untill here
 
 
-
-// Sophie working up untill here
+}

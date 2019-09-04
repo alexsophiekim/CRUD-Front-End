96cce299e496ee02eb62186af6d55eb569bc9d77
Sophie Kim <270010565@bun2-206-16.acg.ac.nz>
2019-09-05 11:21:14 +1200

    appended card working

--- js/script.js
+++ js/script.js
@@ -23,18 +23,19 @@ $.ajax({
 
 
 $('.btn-success').click(function(){
-  $('#cardContainer').empty();
-  for (var i = 0; i < data.length; i++) {
-      let layout = `<div class="col-12 mb-3">
-                     <div class="card">
-                     <img id="workImg" src="" class="card-img-top">
-                       <div class="card-body">
-                         <div id="worktitle" class="card-title"><h5 class="card-title text-center"></h5></div>
-                         <div class="card-body">
-                           <p id="workAuthor" class="card-text text-center"></p>
-                         </div>
-                       </div>
-                     </div>
-                    </div>`
-      $('#cardContainer').append(layout);
+  $('#cardContainer').append(
+    `<div class="col-12 mb-3">
+       <div class="card">
+       <img id="workImg" src="" class="card-img-top">
+         <div class="card-body">
+           <div id="worktitle" class="card-title"><h5 class="card-title text-center"></h5></div>
+           <div class="card-body">
+             <p id="workAuthor" class="card-text text-center"></p>
+           </div>
+         </div>
+       </div>
+      </div>`);
+
+
+
 });

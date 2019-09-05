2367483d9ed219b78e2a37bc4eb8ba47f213b6d4
Larissa <cloudcity101@gmail.com>
2019-09-05 19:05:50 +1200

    beginning add and display

--- js/script.js
+++ js/script.js
@@ -21,6 +21,36 @@ $.ajax({
     }
 });
 
+WorkCards = () => {
+  $.ajax({
+    url: `${url}/view`,
+    type: 'GET',
+    dataType: 'json',
+    success:function(data) {
+      $("#cardContainer").empty();
+
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        $('#cardContainer').append(
+          `<div class="card col-3 mr-2 mb-3">
+            <img id="workImg" src="https://via.placeholder.com/150" class="card-img-top">
+              <div class="card-body">
+               <div id="worktitle" class="card-title"><h5 class="card-title text-center">${data[i].workName}</h5></div>
+               <div class="card-body">
+                <p id="workAuthor" class="card-text text-center">${data[i].workAuthor}</p>
+               </div>
+              </div>
+          </div>`
+        );
+      }
+    },
+    error: function(err){
+      console.log(err);
+      console.log('something went wrong with getting all the products');
+    }
+  })
+}
+
 $('#submittedWork').click(function(){
   event.preventDefault();
 
@@ -29,15 +59,40 @@ $('#submittedWork').click(function(){
   let authorURL = $('#authorURL').val();
   let imageURL = $('#imageURL').val();
 
-  $('#cardContainer').append(
-    `<div class="card col-3 mr-2 mb-3">
-       <img id="workImg" src="https://via.placeholder.com/150" class="card-img-top">
-         <div class="card-body">
-           <div id="worktitle" class="card-title"><h5 class="card-title text-center">` + workItem +`</h5></div>
-           <div class="card-body">
-             <p id="workAuthor" class="card-text text-center">` + workAuthor +`</p>
-           </div>
-         </div>
-       </div>`
-    );
+  if(workItem.length === 0){
+    console.log('Please give a title for this project');
+  }else if(workAuthor.length === 0){
+    console.log('Please give an image URL for this project');
+  }else if(authorURL.length === 0){
+    console.log('Please give an author for this project');
+  }else if(imageURL.length === 0){
+    console.log('Please give a URL for this project');
+  }else {
+
+    $.ajax({
+      url: `${url}/add`,
+      type: 'POST',
+      data: {
+        workName: workName,
+        workAuthor: workAuthor,
+        workImg: workImg,
+        authorURL: authorURL,
+      },
+      success:function(result){
+        let workItem = $('#workItem').val(null);
+        let workAuthor = $('#workAuthor').val(null);
+        let authorURL = $('#authorURL').val(null);
+        let imageURL = $('#imageURL').val(null);
+
+        console.log(workItem);
+        console.log(workAuthor);
+        console.log(authorURL);
+        console.log(imageURL);
+      },
+      error:function(err){
+        console.log(err);
+        console.log('oops, something went wrong');
+      }
+    });
+  }
 });
